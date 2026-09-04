1f5cd0eb0f14e29552a3af3bf15b450c0b9111d3
Kavuru-Lavanya <kavurulavanya183@gmail.com>
2026-09-04 20:26:54 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left=0,right=people.length-1;
+        int count=0;
+        while(left<=right){
+                int sum=people[left]+people[right];
+                if(sum<=limit){
+                    left++;
+                }
+                right--;
+                count++;
+        }
+        return count;
+    }
+}
