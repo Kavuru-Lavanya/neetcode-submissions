ce90a9dc8b7e9353d7c77af63b85601f7585effa
Kavuru-Lavanya <kavurulavanya183@gmail.com>
2026-09-13 21:18:35 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low=0,high=x;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if((mid*mid)==x){
+                return (int)mid;
+            }
+            else if((mid*mid)>x){
+                high=(int)mid-1;
+            }
+            else{
+                low=(int)mid+1;
+            }
+        }
+        return (int)high;
+    }
+}
